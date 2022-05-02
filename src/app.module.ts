11817e555476f8804e3681221f49f708cf2fe54e
Mikkel Mouridsen <mikkelmouridsen@me.com>
2022-05-02 11:07:13 +0200

    Added static file server

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { doc } from 'prettier';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, DatabaseModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    DatabaseModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'content'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
